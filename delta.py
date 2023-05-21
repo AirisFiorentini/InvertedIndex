6462552e8f46aa27e832d89eed9f1c03fece5caa
Tokareva Kseniia <airisfior05@duck.com>
2023-05-21 21:29:05 +0300

    Fixed delta

--- delta.py
+++ delta.py
@@ -7,48 +7,100 @@ from pympler import asizeof
 from BitVector import BitVector
 import sys
 
-# from gamma import *
-from gamma import create_inverted_index
+# Функции для создания индекса
+def tokenize(text):
+    return re.findall(r'\b\w+\b', text.lower())
 
-def delta_code_elias(int_value):
-    result = ""
-    binary = str(bin(int_value))[2:]
-    binary_len = len(binary)
-    len_binary_len_binary = str(bin(binary_len))[2:]
-    for i in range(len(len_binary_len_binary) - 1):
-        result += "0"
-    result += "1"
-    result += len_binary_len_binary[1:]
-    result += binary[1:]
-    return result 
+def create_inverted_index(csv_filename): # инвертированный индекс
+    inverted_index = defaultdict(set)
+    with open(csv_filename, 'r', encoding="utf8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            post_id = int(row['post_id'])
+            text = row['text']
+            for word in tokenize(text):
+                inverted_index[word].add(post_id)
+    return inverted_index
 
-def delta_encode_bitvector(numbers):
+
+def delta_encode_bitvector2(numbers):
+    numbers = sorted(numbers)
     prev = 0
     delta_encoded = BitVector(size=0)
     for number in sorted(numbers):
         delta = number - prev
         binary = BitVector(intVal=delta)
-        unary = BitVector(intVal=len(binary) - 1)
-        unary.pad_from_left(1)  # Append leading 1
+        unary = BitVector(intVal=len(binary))
+        unary.pad_from_left(len(unary)-1)
         delta_encoded += unary
+        delta_encoded += BitVector(intVal=1)
+        delta_encoded += unary[1:]
         delta_encoded += binary[1:]  # Append binary representation without leading 1
         prev = number
     return delta_encoded
 
-def delta_decode_bitvector(delta_encoded):
+
+
+def delta_decode_bitvector2(delta_encoded):
     numbers = []
     total = 0
     i = 0
     while i < len(delta_encoded):
-        unary_len = delta_encoded[i:].intValue() + 1  # Count unary length
+        unary_len = 1
+        while delta_encoded[i + unary_len] == "0":
+            unary_len += 1
         i += unary_len
-        binary = BitVector(intVal=1, size=1)
-        binary += delta_encoded[i:i+unary_len]  # Append binary representation
-        delta = binary.intValue()
+        binary = delta_encoded[i:i+unary_len]
+        delta = int("1" + binary, 2)
         total += delta
         numbers.append(total)
         i += unary_len
     return numbers
+
+def delta_encode_bitvector(numbers):
+    numbers = sorted(numbers)
+    prev = 0
+    delta_encoded = BitVector(size=0)
+    for number in sorted(numbers):
+        delta = number - prev
+        binary = BitVector(intVal=delta)
+        unary = BitVector(intVal=len(binary))
+        for i in range(len(unary) - 1):
+            delta_encoded += BitVector(intVal=0)
+        delta_encoded += BitVector(intVal=1)
+        delta_encoded += unary[1:]
+        delta_encoded += binary[1:]  # Append binary representation without leading 1
+        prev = number
+    return delta_encoded
+
+
+def delta_decode_bitvector(delta_encoded):
+    numbers = []
+    i = 0
+    last_decoded = 0
+    while i < len(delta_encoded):
+        zeros = 0
+        while True:
+            if delta_encoded[i:i+1] == BitVector(intVal=1):
+                break
+            zeros += 1
+            i += 1
+        i += 1
+        next_m_bits = BitVector(size=0)
+        for j in range(zeros):
+            next_m_bits += delta_encoded[i:i+1]
+            i += 1
+        l_value = 2 ** zeros + next_m_bits.intValue()
+        next_l_bits = BitVector(size=0)
+        for j in range(l_value - 1):
+            next_l_bits += delta_encoded[i:i+1]
+            i += 1
+        value = 2 ** (l_value - 1) + next_l_bits.intValue()
+        value += last_decoded
+        last_decoded = value
+        numbers.append(value)
+    return numbers
+
 
 def delta_encode_bitvector_compressed(inverted_index):
     delta_compressed_index = {}
@@ -62,29 +114,13 @@ def delta_decode_bitvector_compressed(delta_compressed_index):
         delta_decompressed_index[word] = delta_decode_bitvector(delta_encoded)
     return delta_decompressed_index
 
-def delta_decode_bitvector(delta_encoded):
-    numbers = []
-    total = 0
-    i = 0
-    while i < len(delta_encoded):
-        unary_len = delta_encoded[i:].intValue() + 1  # Count unary length
-        if i+unary_len > len(delta_encoded):
-            break
-            raise Exception(f"Index out of range: i={i}, unary_len={unary_len}, length of delta_encoded={len(delta_encoded)}")
-        binary = BitVector(intVal=1, size=1)
-        binary += delta_encoded[i : i + unary_len]  # Append binary representation without leading 1
-        delta = binary.intValue()
-        total += delta
-        numbers.append(total)
-        i += unary_len
-    return numbers
-
 print()
 print("Delta:")
 inverted_index = create_inverted_index('posts_SPbU.csv') # ('test_files/empty_file.csv') # ('test_files/empty_file.csv') ('posts_MGU.csv')
 delta_bitvector_compressed_index = delta_encode_bitvector_compressed(inverted_index)
 delta_bitvector_size = asizeof.asizeof(delta_bitvector_compressed_index) 
+print(f"Delta bitvector compressed index size: {delta_bitvector_size}")
+
 delta_decompressed_index = delta_decode_bitvector_compressed(delta_bitvector_compressed_index)
 print(inverted_index['покоряем'])
 print(delta_decompressed_index['покоряем'])
-print(f"Delta bitvector compressed index size: {delta_bitvector_size}")
